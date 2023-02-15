0056e485517975ad5bf7305dc7fd20234375f1f6
Alvaro <varo_ccx@hotmail.com>
2023-02-15 08:53:46 +0100

    Fix main.tf

--- main.tf
+++ main.tf
@@ -13,11 +13,12 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+
 resource "aws_instance" "app_server" {
-  ami           = "ami-0b752bf1df193a6c4"
-  instance_type = "t2.micro"
-  key_name	= "clave-lucatic.pem"
-  vpc_security_group_ids = "sg-013c0f1d5466fd440"
+  ami                    = "ami-0b752bf1df193a6c4"
+  instance_type          = "t2.micro"
+  key_name               = "clave-lucatic"
+  vpc_security_group_ids = ["sg-013c0f1d5466fd440"]
 
   tags = {
     Name = "hello-terraform"
