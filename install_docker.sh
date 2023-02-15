c743ebdcd854212f5b878d5c1b147ce98caaaff9
Alvaro <varo_ccx@hotmail.com>
2023-02-15 12:42:48 +0100

    Updated main.tf to user_data

--- install_docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo yum update -y && sudo yum install -y
-sudo amazon-linux-extras install -y docker
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-sudo pip3 install docker-compose
-
