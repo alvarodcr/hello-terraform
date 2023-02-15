8c6458efdb93deb9618bbd6a29f23825bb0312a0
Alvaro <varo_ccx@hotmail.com>
2023-02-15 09:55:17 +0100

    Uploaded init.sh

--- init.sh
+++ init.sh
@@ -7,5 +7,5 @@ pip3 install docker-compose
 mkdir /home/ec2-user/app
 cd /home/ec2-user/app
 wget https://raw.githubusercontent.com/alvarodcr/hello-2048/main/docker-compose.yml
-wget https://raw.githubusercontent.com/alvarodcr/hello-2048/main/Dockerfile.yml
+docker pull ghcr.io/alvarodcr/hello-2048/hello2048:v1
 docker-compose up -d
